406cbff2461359687ece99acd72301b07e0cf6ff
James Hrivnak <jameshrivnak@Jamess-Mac-mini.local>
2025-03-05 10:43:14 -0700

    copy changes

--- src/devices/OpalIceMaker/Managers/OpalMetadataSvcManager.ts
+++ src/devices/OpalIceMaker/Managers/OpalMetadataSvcManager.ts
@@ -18,10 +18,10 @@ export class OpalMetadataSvcManager extends OpalDeviceBase {
     this.service = this.accessory.getService('')!
     // Metadata for Device Form, Opal Production Limit
     const opalProductionLimitQueryStr = 'l=Production_Duration_Minutes&i=opalProductionLimit&t=number&d=0&p=Number._0_for_Infinite'
-    const opalHKCNotificationsPathQueryStr = 'l=HKC_Ice_Bucket_Full_Notifications_Path&i=oplHKCIceBucketFullNotificationPath'
+    const opalHKCNotificationsPathQueryStr = 'l=HKC_Ice_Bucket_Full_Notification_Path&i=oplHKCIceBucketFullNotificationPath'
     const opalHKCCompletionNotificationPathQueryStr = 'l=HKC_Progress_Complete_Notification_Path&i=oplHKCProgressCompleteNotificationPath'
     const opalHKCFilterMaintenanceNotificationPathQueryStr = 'l=HKC_Filter_Maintenace_Notification_Path&i=oplHKCFilterMaintenanceNotificationPath'
-    const opalHKCAddWaterNotificationPathQueryStr = 'l=HKC_Add_water_Notification_Path&i=oplHKCAddWaterNotificationPath'
+    const opalHKCAddWaterNotificationPathQueryStr = 'l=HKC_Add_Water_Notification_Path&i=oplHKCAddWaterNotificationPath'
 
     const fullQueryStr = [
       opalProductionLimitQueryStr,
